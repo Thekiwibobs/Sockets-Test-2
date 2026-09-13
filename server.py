ec8941cf13fd4e9c5229ede60170dfe7bd65f06f
Thekiwibobs <kiwibobbins@gmail.com>
2026-09-13 18:15:13 +0100

    added f string where there was meant to be one

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ def handle(client):
 def recieve():
     while True:
         client, address = server.accept()
-        print("connected with {str(address)}")
+        print(f"connected with {str(address)}")
 
         client.send("NICK".encode("ascii"))
         nickname = client.recv(1024).decode("ascii")
